49fe86454a4119f90c245a6be447881c075fa8ad
nataliakloc96-ui <natalia.kloc96@gmail.com>
2026-04-28 23:02:47 +0200

    Refactor main.py to improve structure and add SSL

--- main.py
+++ main.py
@@ -31,7 +31,8 @@ conn = psycopg2.connect(
     user=os.getenv("DB_USER"),
     password=os.getenv("DB_PASS"),
     host=os.getenv("DB_HOST"),
-    port=os.getenv("DB_PORT")
+    port=os.getenv("DB_PORT"),
+    sslmode="require"
 )
 
 
