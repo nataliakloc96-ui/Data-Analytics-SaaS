b34db66e63f1db702a182aefbb7d5d9a9d2a60d0
nataliakloc96-ui <natalia.kloc96@gmail.com>
2026-04-28 23:07:48 +0200

    Refactor FastAPI application structure and endpoints

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, UploadFile, File
+from fastapi import FastAPI, UploadFile, File, Depends
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from jose import jwt 
