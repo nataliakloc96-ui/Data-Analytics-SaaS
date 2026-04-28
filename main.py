f36c81e9f29bbd0918728127cde1aec95bdaedcc
nataliakloc96-ui <natalia.kloc96@gmail.com>
2026-04-28 22:00:57 +0200

    Refactor FastAPI app and endpoints

--- main.py
+++ main.py
@@ -25,11 +25,11 @@ class User(BaseModel):
     password: str
 
 conn = psycopg2.connect(
-    dbname=os.getenv["DB_NAME"],
-    user=os.getenv["DB_USER"],
-    password=os.getenv["DB_PASS"],
-    host=os.getenv["DB_HOST"],
-    port=os.getenv["DB_PORT"]
+    dbname=os.getenv("DB_NAME"),
+    user=os.getenv("DB_USER"),
+    password=os.getenv("DB_PASS"),
+    host=os.getenv("DB_HOST"),
+    port=os.getenv("DB_PORT")
 )
 
 
